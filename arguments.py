a44b43c41dc701fb131b083d75171bf26b5e62b2
GmanStak <836375453@qq.com>
2023-09-28 09:06:04 +0800

    Update arguments.py

--- arguments.py
+++ arguments.py
@@ -3,7 +3,7 @@ import argparse
 
 def get_args():
     parser = argparse.ArgumentParser(description='OceanStore 9000 options!')
-    parser.add_argument("-a","--address",type=str,default='localhost')
+    parser.add_argument("-a","--address",type=str,default='0.0.0.0')
     parser.add_argument("-p","--port",type=str,default=9099)
     parser.add_argument("-c", "--config", type=str, default="./conf/config.ini")
     args = parser.parse_args()
