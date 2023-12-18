6466ae032ebb698e38bf27d222bea38e7d9ce9c6
GmanStak <836375453@qq.com>
2023-12-18 10:53:51 +0800

    Update exporter.py
    添加路由认证

--- exporter.py
+++ exporter.py
@@ -1,17 +1,31 @@
 import prometheus_client
 from flask import Response, Flask
+from flask_basicauth import BasicAuth
 from gevent import pywsgi
 from prom_metrics import *
 from collecter import metics
-from arguments import get_args
+from arguments import get_args,get_auth
+
+args = get_args()
+
+auth_path = args.auth_config
+auth_host,auth_passwd = get_auth(auth_path)
 
 app = Flask(__name__)
 
+app.config["BASIC_AUTH_USERNAME"] = auth_host
+app.config["BASIC_AUTH_PASSWORD"] = auth_passwd
+basic_auth = BasicAuth(app)
+
+
+
 @app.route('/')
+@basic_auth.required
 def index():
     return  "<h1>OceanStore 9000 Metrics!</h1><br> <a href='metrics'>Metrics</a>"
 
 @app.route('/metrics')
+@basic_auth.required
 def get_metrics():
     clear_metrics()
     metics()
